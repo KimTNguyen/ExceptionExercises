29dffc3a3762f673f160d21213449bf25c8f62f3
KimTNguyen <kimthy1234@yahoo.com>
2016-09-13 23:21:42 -0700

    catching any exception

---
+++ src/Exercise9.java
@@ -0,0 +1,41 @@
+/**
+ * Create three new types of exceptions. Write a class with a method that throws all three.
+ * In main(), call the method but only use a single catch clause that will catch all three
+ * types of exception
+ *
+ * @author Kim Thy Nguyen
+ * @version %I%, %G%
+ * @since 1.0
+ */
+
+public class Exercise9 {
+	private static void throwExceptions() throws MyException1, MyException2, MyException3 {
+		throw new MyException1("Exception1");
+	}
+
+	public static void main() {
+		try {
+			throwExceptions();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class MyException1 extends Exception {
+	MyException1(String msg) {
+		super(msg);
+	}
+}
+
+class MyException2 extends Exception {
+	MyException2(String msg) {
+		super(msg);
+	}
+}
+
+class MyException3 extends Exception {
+	MyException3(String msg) {
+		super(msg);
+	}
+}
